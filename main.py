c88bf7c0200cd18a9bb2d7b7f8ba7d1fd4340b5f
sasha <sasha9495@list.ru>
2025-07-20 12:11:28 +1000

    функция

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ print("Hello world")
 
 def func():
     print("123")
+
+def bruteforce():
+    print("bruteforce initiated")
+
+import threading
+import time
+
+def annoy():
+    while True:
+        print("Хотите кофе?")
+        time.sleep(5)
